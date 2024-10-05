c8f7d0177ce137020f67e31c468ecd8daa77e0b9
DanielaOdhis <daniela.odhiambo@students.jkuat.ac.ke>
2024-10-05 10:31:53 +0300

    Add project files.

---
+++ GenericCalculator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace GenericCalculator
+{
+    // Generic class for Calculator
+    public class Calculator<T> where T : struct
+    {
+        // Generic method for addition
+        public T Add(T a, T b)
+        {
+            return (dynamic)a + (dynamic)b;
+        }
+
+        // Generic method for subtraction
+        public T Subtract(T a, T b)
+        {
+            return (dynamic)a - (dynamic)b;
+        }
+
+        // Generic method for multiplication
+        public T Multiply(T a, T b)
+        {
+            return (dynamic)a * (dynamic)b;
+        }
+
+        // Generic method for division
+        public T Divide(T a, T b)
+        {
+            if ((dynamic)b == 0)
+            {
+                throw new DivideByZeroException("Cannot divide by zero");
+            }
+            return (dynamic)a / (dynamic)b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator<double> calculator = new Calculator<double>();
+            double a = GetInput<double>("Enter first double: ");
+            double b = GetInput<double>("Enter second double: ");
+            PerformCalculations(calculator, a, b);
+        }
+
+        // Generic method to get user input and convert it to the required type
+        static T GetInput<T>(string prompt) where T : struct
+        {
+            Console.WriteLine(prompt);
+            string input = Console.ReadLine();
+            return (T)Convert.ChangeType(input, typeof(T));
+        }
+
+        // Method to perform calculations
+        static void PerformCalculations<T>(Calculator<T> calculator, T a, T b) where T : struct
+        {
+            Console.WriteLine($"Addition: {calculator.Add(a, b)}");
+            Console.WriteLine($"Subtraction: {calculator.Subtract(a, b)}");
+            Console.WriteLine($"Multiplication: {calculator.Multiply(a, b)}");
+
+            try
+            {
+                Console.WriteLine($"Division: {calculator.Divide(a, b)}");
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
---
+++ GenericCalculator/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("GenericCalculator")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenericCalculator")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a4f480d9-b32d-4db7-a8c5-277d096006e3")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
